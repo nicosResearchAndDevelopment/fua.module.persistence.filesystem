94f0141731eb69ac39514b97cfef88ed758a41d9
Simon Petrac <pfoerdie@gmail.com>
2021-01-13 15:59:14 +0100

    added describe

--- test/StoreFactoryInterface.test.js
+++ test/StoreFactoryInterface.test.js
@@ -4,4 +4,8 @@ const
 	dataFactory = require('../../module.persistence/src/module.persistence.js'),
 	datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js');
 
-// TODO
+describe('module.persistence.filesystem : StoreFactoryInterface', function() {
+
+	// TODO
+
+});
