2c794f4029250c3bd7ead41600f155c6928666c5
Simon Petrac <pfoerdie@gmail.com>
2021-01-13 16:10:59 +0100

    fix

--- src/module.persistence.filesystem.js
+++ src/module.persistence.filesystem.js
@@ -1,5 +1,14 @@
 const
-	dataFactory = require('../../module.persistence/src/module.persistence.js'),
-	datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js');
+	// dataFactory = require('../../module.persistence/src/module.persistence.js'),
+	// datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js'),
+	FileSystemStore = require('./FileSystemStore.js');
 
-// TODO
+/**
+ * @param {NamedNode} graph
+ * @para {string} directory
+ * @returns {FileSystemStore}
+ */
+exports.store = function(graph, directory) {
+	throw new Error('currently not implemented');
+	// return new FileSystemStore(graph, directory);
+};
