e02aca291b2e1bb87fa7a70371a6846d8a974dcc
Simon Petrac <pfoerdie@gmail.com>
2021-02-24 11:42:38 +0100

    added compatibility error until next update

--- src/module.persistence.filesystem.js
+++ src/module.persistence.filesystem.js
@@ -1,14 +1,16 @@
+throw new Error('module.persistence.filesystem : not compatible with current module.persistence');
+
 const
-	// dataFactory = require('../../module.persistence/src/module.persistence.js'),
-	// datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js'),
-	FileSystemStore = require('./FileSystemStore.js');
+    // dataFactory = require('../../module.persistence/src/module.persistence.js'),
+    // datasetFactory = require('../../module.persistence.inmemory/src/module.persistence.inmemory.js'),
+    FileSystemStore = require('./FileSystemStore.js');
 
 /**
  * @param {NamedNode} graph
  * @para {string} directory
  * @returns {FileSystemStore}
  */
-exports.store = function(graph, directory) {
-	throw new Error('currently not implemented');
-	// return new FileSystemStore(graph, directory);
+exports.store = function (graph, directory) {
+    throw new Error('currently not implemented');
+    // return new FileSystemStore(graph, directory);
 };
