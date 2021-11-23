3e218ed988a6070ad2b1005cb8fc7ee9c39426d3
Simon Petrac <pfoerdie@gmail.com>
2021-11-23 18:19:35 +0100

    finished FileSystemStore -> tests may be still necessary

--- src/module.persistence.filesystem.js
+++ src/module.persistence.filesystem.js
@@ -16,42 +16,82 @@ const
 
 class FilesystemStore extends DataStore {
 
-    #ready        = false;
-    #readyPromise = null;
+    #ready         = false;
+    #readyPromise  = null;
     /** @type {Map<string, FileDescription>} */
-    #files        = new Map();
-
-    // #update(id) {
-    //     const file = this.#files.get(id);
-    //     if (file.updater) clearTimeout(file.updater);
-    //     file.updater = setTimeout(async () => {
-    //         try {
-    //             const content = await rdf.serializeDataset(file.dataset, file.format);
-    //             await fs.writeFile(file.identifier, content);
-    //         } catch (err) {
-    //             this.emit('error', err);
-    //         } finally {
-    //             clearTimeout(file.updater);
-    //             delete file.updater;
-    //         }
-    //     }, 1e3);
-    // } // FilesystemStore##update
+    #files         = new Map();
+    #defaultFile   = '';
+    #updateTimesMS = new Map();
+    #updateDelayMS = 1e3;
 
     constructor(options, factory) {
         super(options, factory);
+        assert(options.load, 'FilesystemStore#constructor : expected options.load to contain a load config');
+        assert(options.default, 'FilesystemStore#constructor : expected options.default to be a string');
+        this.#defaultFile  = options.default;
         this.#readyPromise = (async () => {
-            const resultArr = await rdf.loadDataFiles(options, factory);
+            const resultArr = await rdf.loadDataFiles(options.load, factory);
             for (let file of resultArr) {
                 if (file.dataset) {
+                    assert(!this.#files.has(file.id), 'FilesystemStore#constructor : expected file IDs to be unique');
                     this.#files.set(file.id, file);
                 }
             }
+            assert(this.#files.size > 0, 'FilesystemStore#constructor : expected at least one file to be loaded');
+            assert(this.#files.has(options.default), 'FilesystemStore#constructor : expected files to contain the default');
         })();
         this.#readyPromise.then(() => {
             this.#ready        = true;
             this.#readyPromise = null;
         });
     } // FilesystemStore#constructor
+
+    #scheduleFileUpdate(fileId) {
+        console.log('schedule updater for ' + fileId);
+        if (this.#updateTimesMS.has(fileId)) {
+            this.#updateTimesMS.set(fileId, Date.now() + this.#updateDelayMS);
+        } else {
+            const file = this.#files.get(fileId);
+            this.#updateTimesMS.set(fileId, Date.now() + this.#updateDelayMS);
+            fs.open(file.identifier, 'w').then((fileHandle) => {
+                let updateMS;
+                const startUpdater = () => {
+                    console.log('started updater for ' + fileId);
+                    updateMS = this.#updateTimesMS.get(fileId);
+                    setTimeout(async () => {
+                        try {
+                            updateMS = this.#updateTimesMS.get(fileId);
+                            if (Date.now() < updateMS) {
+                                startUpdater();
+                                return;
+                            }
+
+                            const fileContent = await rdf.serializeDataset(file.dataset, file.format);
+                            updateMS          = this.#updateTimesMS.get(fileId);
+                            if (Date.now() < updateMS) {
+                                startUpdater();
+                                return;
+                            }
+
+                            await fileHandle.writeFile(fileContent);
+                            updateMS = this.#updateTimesMS.get(fileId);
+                            if (Date.now() < updateMS) {
+                                startUpdater();
+                                return;
+                            }
+
+                            console.log('finished updater for ' + fileId);
+                            this.#updateTimesMS.delete(fileId);
+                            await fileHandle.close();
+                        } catch (err) {
+                            this.emit('error', err);
+                        }
+                    }, Math.max(0, updateMS - Date.now()));
+                };
+                startUpdater();
+            }).catch(err => this.emit('error', err));
+        }
+    } // FilesystemStore##scheduleFileUpdate
 
     async size() {
         this.#ready || await this.#readyPromise;
@@ -63,35 +103,53 @@ class FilesystemStore extends DataStore {
     } // FilesystemStore#size
 
     async match(subject, predicate, object, graph) {
+        this.#ready || await this.#readyPromise;
         const dataset = await super.match(subject, predicate, object, graph);
+        let fileIterable;
+
         if (graph) {
-            assert(this.factory.isNamedNode(graph), 'FilesystemStore#add : expected graph to be a NamedNode');
-            const file = this.#files.get(graph.value);
-            if (file) {
-                for (let fileQuad of file.dataset.match(subject, predicate, object)) {
-                    const quad = this.factory.quad(fileQuad.subject, fileQuad.predicate, fileQuad.object, graph)
-                    dataset.add(quad);
-                }
+            if (this.factory.isDefaultGraph(graph)) {
+                fileIterable = [this.#files.get(this.#defaultFile)];
+            } else {
+                assert(this.factory.isNamedNode(graph), 'FilesystemStore#match : expected graph to be a NamedNode');
+                fileIterable = [this.#files.get(graph.value)];
             }
         } else {
-            for (let file of this.#files.values()) {
-                const graph = this.factory.namedNode(file.id);
-                for (let fileQuad of file.dataset.match(subject, predicate, object)) {
-                    const quad = this.factory.quad(fileQuad.subject, fileQuad.predicate, fileQuad.object, graph)
-                    dataset.add(quad);
-                }
+            fileIterable = this.#files.values();
+        }
+
+        for (let file of fileIterable) {
+            const fileGraph = (file.id === this.#defaultFile)
+                ? this.factory.defaultGraph()
+                : this.factory.namedNode(file.id);
+            for (let fileQuad of file.dataset.match(subject, predicate, object)) {
+                const quad = this.factory.quad(
+                    fileQuad.subject,
+                    fileQuad.predicate,
+                    fileQuad.object,
+                    fileGraph
+                );
+                dataset.add(quad);
             }
         }
+
         return dataset;
     } // FilesystemStore#match
 
     async add(quads) {
+        this.#ready || await this.#readyPromise;
         const
             quadArr    = await super.add(quads),
             quadArrMap = new Map();
+
         for (let quad of quadArr) {
-            assert(this.factory.isNamedNode(quad.graph), 'FilesystemStore#add : expected quad.graph to be a NamedNode');
-            const file = this.#files.get(quad.graph.value);
+            let file;
+            if (this.factory.isDefaultGraph(quad.graph)) {
+                file = this.#files.get(this.#defaultFile);
+            } else {
+                assert(this.factory.isNamedNode(quad.graph), 'FilesystemStore#add : expected quad.graph to be a NamedNode');
+                file = this.#files.get(quad.graph.value);
+            }
             assert(file, 'FilesystemStore#add : expected quad to contain a known graph');
             if (quadArrMap.has(file)) {
                 quadArrMap.get(file).push(quad);
@@ -99,49 +157,185 @@ class FilesystemStore extends DataStore {
                 quadArrMap.set(file, [quad]);
             }
         }
+
         let added = 0;
         for (let [file, fileQuadArr] of quadArrMap.entries()) {
+            let fileEdited = false;
             for (let quad of fileQuadArr) {
                 const fileQuad = this.factory.quad(quad.subject, quad.predicate, quad.object);
                 if (!file.dataset.has(fileQuad)) {
                     file.dataset.add(fileQuad);
                     this.emit('added', quad);
                     added++;
+                    fileEdited = true;
                 }
             }
-            // TODO schedule file for update
+            if (fileEdited) this.#scheduleFileUpdate(file.id);
         }
+
         return added;
     } // FilesystemStore#add
 
     async addStream(stream) {
-        assert(false, 'TODO : not implemented');
-        // const quadStream = await super.addStream(stream);
-        // TODO
+        this.#ready || await this.#readyPromise;
+        const quadStream = await super.addStream(stream);
+        let added        = 0;
+        quadStream.on('data', (quad) => {
+            let file;
+            if (this.factory.isDefaultGraph(quad.graph)) {
+                file = this.#files.get(this.#defaultFile);
+            } else if (this.factory.isNamedNode(quad.graph)) {
+                file = this.#files.get(quad.graph.value);
+            } else {
+                return;
+            }
+            const fileQuad = this.factory.quad(quad.subject, quad.predicate, quad.object);
+            if (!file.dataset.has(fileQuad)) {
+                file.dataset.add(fileQuad);
+                this.emit('added', quad);
+                added++;
+                this.#scheduleFileUpdate(file.id);
+            }
+        });
+        await new Promise(resolve => quadStream.on('end', resolve));
+        return added;
     } // FilesystemStore#addStream
 
     async delete(quads) {
-        assert(false, 'TODO : not implemented');
-        // const quadArr = await super.delete(quads);
-        // TODO
+        this.#ready || await this.#readyPromise;
+        const
+            quadArr    = await super.add(quads),
+            quadArrMap = new Map();
+
+        for (let quad of quadArr) {
+            let file;
+            if (this.factory.isDefaultGraph(quad.graph)) {
+                file = this.#files.get(this.#defaultFile);
+            } else {
+                assert(this.factory.isNamedNode(quad.graph), 'FilesystemStore#delete : expected quad.graph to be a NamedNode');
+                file = this.#files.get(quad.graph.value);
+            }
+            assert(file, 'FilesystemStore#delete : expected quad to contain a known graph');
+            if (quadArrMap.has(file)) {
+                quadArrMap.get(file).push(quad);
+            } else {
+                quadArrMap.set(file, [quad]);
+            }
+        }
+
+        let deleted = 0;
+        for (let [file, fileQuadArr] of quadArrMap.entries()) {
+            let fileEdited = false;
+            for (let quad of fileQuadArr) {
+                const fileQuad = this.factory.quad(quad.subject, quad.predicate, quad.object);
+                if (file.dataset.has(fileQuad)) {
+                    file.dataset.delete(fileQuad);
+                    this.emit('deleted', quad);
+                    deleted++;
+                    fileEdited = true;
+                }
+            }
+            if (fileEdited) this.#scheduleFileUpdate(file.id);
+        }
+
+        return deleted;
     } // FilesystemStore#delete
 
     async deleteStream(stream) {
-        assert(false, 'TODO : not implemented');
-        // const quadStream = await super.deleteStream(stream);
-        // TODO
+        this.#ready || await this.#readyPromise;
+        const quadStream = await super.addStream(stream);
+        let deleted      = 0;
+        quadStream.on('data', (quad) => {
+            let file;
+            if (this.factory.isDefaultGraph(quad.graph)) {
+                file = this.#files.get(this.#defaultFile);
+            } else if (this.factory.isNamedNode(quad.graph)) {
+                file = this.#files.get(quad.graph.value);
+            } else {
+                return;
+            }
+            const fileQuad = this.factory.quad(quad.subject, quad.predicate, quad.object);
+            if (file.dataset.has(fileQuad)) {
+                file.dataset.delete(fileQuad);
+                this.emit('deleted', quad);
+                deleted++;
+                this.#scheduleFileUpdate(file.id);
+            }
+        });
+        await new Promise(resolve => quadStream.on('end', resolve));
+        return deleted;
     } // FilesystemStore#deleteStream
 
     async deleteMatches(subject, predicate, object, graph) {
-        assert(false, 'TODO : not implemented');
-        // await super.deleteMatches(subject, predicate, object, graph);
-        // TODO
+        this.#ready || await this.#readyPromise;
+        await super.deleteMatches(subject, predicate, object, graph);
+        let fileIterable;
+
+        if (graph) {
+            if (this.factory.isDefaultGraph(graph)) {
+                fileIterable = [this.#files.get(this.#defaultFile)];
+            } else {
+                assert(this.factory.isNamedNode(graph), 'FilesystemStore#deleteMatches : expected graph to be a NamedNode');
+                fileIterable = [this.#files.get(graph.value)];
+            }
+        } else {
+            fileIterable = this.#files.values();
+        }
+
+        let deleted = 0;
+        for (let file of fileIterable) {
+            const fileGraph = (file.id === this.#defaultFile)
+                ? this.factory.defaultGraph()
+                : this.factory.namedNode(file.id);
+
+            let fileEdited = false;
+            for (let fileQuad of file.dataset.match(subject, predicate, object)) {
+                const quad = this.factory.quad(
+                    fileQuad.subject,
+                    fileQuad.predicate,
+                    fileQuad.object,
+                    fileGraph
+                );
+                file.dataset.delete(fileQuad);
+                this.emit('deleted', quad);
+                deleted++;
+                fileEdited = true;
+            }
+            if (fileEdited) this.#scheduleFileUpdate(file.id);
+        }
+
+        return deleted;
     } // FilesystemStore#deleteMatches
 
     async has(quads) {
-        assert(false, 'TODO : not implemented');
-        // const quadArr = await super.has(quads);
-        // TODO
+        this.#ready || await this.#readyPromise;
+        const
+            quadArr    = await super.add(quads),
+            quadArrMap = new Map();
+
+        for (let quad of quadArr) {
+            let file;
+            if (this.factory.isDefaultGraph(quad.graph)) {
+                file = this.#files.get(this.#defaultFile);
+            } else if (this.factory.isNamedNode(quad.graph)) {
+                file = this.#files.get(quad.graph.value);
+            }
+            if (!file) return false;
+            if (quadArrMap.has(file)) {
+                quadArrMap.get(file).push(quad);
+            } else {
+                quadArrMap.set(file, [quad]);
+            }
+        }
+
+        for (let [file, fileQuadArr] of quadArrMap.entries()) {
+            for (let quad of fileQuadArr) {
+                const fileQuad = this.factory.quad(quad.subject, quad.predicate, quad.object);
+                if (!file.dataset.has(fileQuad)) return false;
+            }
+        }
+
+        return true;
     } // FilesystemStore#has
 
 } // FilesystemStore
--- test/FilesystemStore.test.js
+++ test/FilesystemStore.test.js
@@ -1,29 +1,31 @@
 const
-    {describe, test, before} = require('mocha'),
-    expect                   = require('expect'),
-    path                     = require('path'),
-    FilesystemStore          = require('../src/module.persistence.filesystem.js');
+    {describe, test, before, after} = require('mocha'),
+    expect                          = require('expect'),
+    path                            = require('path'),
+    FilesystemStore                 = require('../src/module.persistence.filesystem.js');
 
 describe('module.persistence.filesystem', function () {
 
     let store, quad_1, quad_2;
     before('construct a FilesystemStore and two quads', function () {
         store  = new FilesystemStore({
-            '@id':            'file://empty.ttl',
-            'dct:identifier': path.join(__dirname, 'data/empty.ttl'),
-            'dct:format':     'text/turtle'
+            default: 'file://empty.ttl',
+            load:    {
+                '@id':            'file://empty.ttl',
+                'dct:identifier': path.join(__dirname, 'data/empty.ttl'),
+                'dct:format':     'text/turtle'
+            }
         });
         quad_1 = store.factory.quad(
             store.factory.namedNode('http://example.com/subject'),
             store.factory.namedNode('http://example.com/predicate'),
-            store.factory.namedNode('http://example.com/object'),
-            store.factory.namedNode('file://empty.ttl')
+            store.factory.namedNode('http://example.com/object')
         );
         quad_2 = store.factory.quad(
             quad_1.subject,
             quad_1.predicate,
-            store.factory.literal('Hello World', 'en'),
-            store.factory.namedNode('file://empty.ttl')
+            store.factory.literal('Hello World', 'en')
+            // store.factory.namedNode('file://empty.ttl')
         );
     });
 
@@ -38,6 +40,42 @@ describe('module.persistence.filesystem', function () {
         expect(await store.add(quad_2)).toBeFalsy();
     });
 
-    // TODO
+    test('should match the two added quads by their subject', async function () {
+        /** @type {Dataset} */
+        const result = await store.match(quad_1.subject);
+        expect(result.has(quad_1)).toBeTruthy();
+        expect(result.has(quad_2)).toBeTruthy();
+    });
+
+    test('should currently have a size of 2', async function () {
+        expect(await store.size()).toBe(2);
+    });
+
+    test('should delete the first quad once', async function () {
+        expect(await store.delete(quad_1)).toBeTruthy();
+        expect(await store.delete(quad_1)).toBeFalsy();
+    });
+
+    test('should only have the second quad stored', async function () {
+        expect(await store.has(quad_1)).toBeFalsy();
+        expect(await store.has(quad_2)).toBeTruthy();
+    });
+
+    test('should match the remaining quad by its object', async function () {
+        /** @type {Dataset} */
+        const result = await store.match(null, null, quad_2.object);
+        expect(result.has(quad_1)).toBeFalsy();
+        expect(result.has(quad_2)).toBeTruthy();
+    });
+
+    test('should have a size of 0, after it deleted the second quad', async function () {
+        await store.delete(quad_2);
+        expect(await store.size()).toBe(0);
+    });
+
+    after('wait a sec before finishing', async function () {
+        this.timeout(3e3);
+        await new Promise(resolve => setTimeout(resolve, 2e3));
+    });
 
 });
